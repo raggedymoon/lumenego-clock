578a17e0d2c82a1581eab62c8ac78f3e8a81bf59
raggedymoon <thepmprogram@gmail.com>
2024-05-16 11:05:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-$(document).ready(function() {
-    var clock = new FlipClock($('#clock'), {
-        clockFace: 'TwentyFourHourClock'
+<script>
+    window.addEventListener('DOMContentLoaded', (event) => {
+        const iframe = document.querySelector('iframe');
+        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+        iframe.style.width = iframe.contentWindow.document.body.scrollWidth + 'px';
     });
-
-    function updateDate() {
-        const now = new Date();
-        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        const dayName = days[now.getDay()];
-        const monthName = months[now.getMonth()];
-        const dateString = `${dayName}, ${monthName} ${now.getDate()}, ${now.getFullYear()}`;
-        $('#date').text(dateString);
-    }
-
-    setInterval(updateDate, 1000);
-    updateDate();
-});
+</script>
