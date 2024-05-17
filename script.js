eb4b950c8de8d1b9810e55bba5c6e81c666a7cf6
raggedymoon <thepmprogram@gmail.com>
2024-05-16 20:47:47 -0400

    script.js

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-function updateTime() {
-    var now = new Date();
-    var hours = now.getHours() % 12 || 12;
-    var minutes = now.getMinutes();
-    var ampm = now.getHours() >= 12 ? 'PM' : 'AM';
-    var timeString = hours + ':' + (minutes < 10 ? '0' : '') + minutes + ' ' + ampm;
+$(document).ready(function() {
+    var clock = new FlipClock($('#clock'), {
+        clockFace: 'HourlyCounter',
+        countdown: false,
+        showSeconds: false
+    });
 
-    document.getElementById('clock').innerText = timeString;
-}
+    function updateTime() {
+        var now = new Date();
+        var hours = now.getHours() % 12 || 12;
+        var minutes = now.getMinutes();
+        var ampm = now.getHours() >= 12 ? 'PM' : 'AM';
+        var timeInSeconds = (hours * 60 * 60) + (minutes * 60); // Set time in seconds
 
-function updateDate() {
-    var now = new Date();
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var dayName = days[now.getDay()];
-    var monthName = months[now.getMonth()];
-    var dateString = `${dayName}, ${monthName} ${now.getDate()}, ${now.getFullYear()}`;
+        clock.setTime(timeInSeconds);
+        clock.start();
+    }
 
-    document.getElementById('date').innerText = dateString;
-}
+    updateTime();
+    setInterval(updateTime, 60000); // Update every minute
 
-setInterval(updateTime, 1000);
-updateTime();
+    function updateDate() {
+        const now = new Date();
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const dayName = days[now.getDay()];
+        const monthName = months[now.getMonth()];
+        const dateString = `${dayName}, ${monthName} ${now.getDate()}, ${now.getFullYear()}`;
+        $('#date').text(dateString);
+    }
 
-setInterval(updateDate, 1000);
-updateDate();
+    setInterval(updateDate, 1000);
+    updateDate();
+});
