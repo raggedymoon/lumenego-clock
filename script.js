c6624202d9c66760c4602b4bfac8d2ee1e6932e3
raggedymoon <thepmprogram@gmail.com>
2024-05-16 21:00:38 -0400

    script.js

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-function updateTime() {
-    var now = new Date();
-    var hours = now.getHours() % 12 || 12;
-    var minutes = now.getMinutes();
-    var ampm = now.getHours() >= 12 ? 'PM' : 'AM';
-    var timeString = hours + ':' + (minutes < 10 ? '0' : '') + minutes + ' ' + ampm;
+$(document).ready(function() {
+    var clock = new FlipClock($('#clock'), {
+        clockFace: 'TwelveHourClock', // Use the 12-hour clock face
+        showSeconds: false
+    });
 
-    document.getElementById('clock').innerText = timeString;
-}
+    function updateDate() {
+        const now = new Date();
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const dayName = days[now.getDay()];
+        const monthName = months[now.getMonth()];
+        const dateString = `${dayName}, ${monthName} ${now.getDate()}, ${now.getFullYear()}`;
+        $('#date').text(dateString);
+    }
 
-function updateDate() {
-    var now = new Date();
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var dayName = days[now.getDay()];
-    var monthName = months[now.getMonth()];
-    var dateString = `${dayName}, ${monthName} ${now.getDate()}, ${now.getFullYear()}`;
-
-    document.getElementById('date').innerText = dateString;
-}
-
-setInterval(updateTime, 1000);
-updateTime();
-
-setInterval(updateDate, 1000);
-updateDate();
+    setInterval(updateDate, 1000);
+    updateDate();
+});
